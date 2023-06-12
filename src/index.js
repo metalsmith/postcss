4e8ec8dfc2395f87f3284b006972c6fa4707d109
Kevin Van Lierde <kevin.van.lierde@gmail.com>
2023-06-12 23:46:23 +0200

    Removes legacy createRequire in favor of dynamic import

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import postcssLib from 'postcss'
 import path from 'path'
-import mod, { createRequire } from 'module'
-
-// support for dynamic imports landed in Node 13.2.0, and was available with --experimental-modules flag in 12.0.0
-// ideally all the loaders should be refactored to be async, and loaded only when the plugin runs
-const req = mod.require || createRequire(import.meta.url)
 
 const defaultOptions = {
   pattern: '**/*.css',
@@ -28,6 +23,24 @@ function normalizeMapOptions(map, development) {
   }
 }
 
+function postcssProcessor(plugins) {
+  const pluginsReady = []
+
+  plugins.forEach((plugin) => {
+    if (typeof plugin === 'string') {
+      pluginsReady.push(import(plugins).then((plugin) => plugin.default({})))
+    } else {
+      Object.keys(plugin).forEach(function (pluginName) {
+        const value = plugin[pluginName]
+        if (value === false) return
+        const pluginOptions = value === true ? {} : value
+        pluginsReady.push(import(pluginName).then((plugin) => plugin.default(pluginOptions)))
+      })
+    }
+  })
+  return Promise.all(pluginsReady).then((plugins) => postcssLib(plugins))
+}
+
 /**
  * @typedef {Object} SourceMapOptions
  * @property {boolean} [inline]
@@ -47,85 +60,78 @@ function normalizeMapOptions(map, development) {
 function postcss(options) {
   options = Object.assign({}, defaultOptions, options || {})
   const pluginsConfig = Array.isArray(options.plugins) ? options.plugins : [options.plugins]
-  const plugins = []
-
-  // Require each plugin, pass its options
-  // and add it to the plugins array.
-  pluginsConfig.forEach(function (pluginsObject) {
-    if (typeof pluginsObject === 'string') {
-      plugins.push(req(pluginsObject)({}))
-    } else {
-      Object.keys(pluginsObject).forEach(function (pluginName) {
-        const value = pluginsObject[pluginName]
-        if (value === false) return
-        const pluginOptions = value === true ? {} : value
-        plugins.push(req(pluginName)(pluginOptions))
-      })
-    }
-  })
-
-  const processor = postcssLib(plugins)
+  const processorReady = postcssProcessor(pluginsConfig)
+  let syntaxReady
+  if (options.syntax) {
+    syntaxReady =
+      typeof options.syntax === 'string'
+        ? import(options.syntax).then((syntax) => syntax.default)
+        : Promise.resolve(options.syntax)
+  }
 
   return function postcss(files, metalsmith, done) {
     const map = normalizeMapOptions(options.map, metalsmith.env('NODE_ENV') === 'development')
     const styles = metalsmith.match(options.pattern, Object.keys(files))
     const debug = metalsmith.debug('@metalsmith/postcss')
     debug('Running with options %O', { ...options, map })
+    Promise.all([processorReady, syntaxReady])
+      .then(([processor, syntax]) => {
+        const promises = []
 
-    const promises = []
+        styles.forEach(function (file) {
+          const contents = files[file].contents.toString()
+          const absolutePath = path.resolve(metalsmith.source(), file)
 
-    styles.forEach(function (file) {
-      const contents = files[file].contents.toString()
-      const absolutePath = path.resolve(metalsmith.source(), file)
+          // if a previous source map has been generated for this file (eg through sass),
+          // pass its contents onto postcss
+          const prevMap = files[`${file}.map`]
+          const mapOpts = map && prevMap ? { prev: prevMap.contents.toString(), ...map } : map
 
-      // if a previous source map has been generated for this file (eg through sass),
-      // pass its contents onto postcss
-      const prevMap = files[`${file}.map`]
-      const mapOpts = map && prevMap ? { prev: prevMap.contents.toString(), ...map } : map
-
-      debug.info('Processing file "%s"', file)
-      const processOptions = {
-        from: absolutePath,
-        to: absolutePath,
-        map: mapOpts
-      }
-      if (options.syntax) {
-        processOptions.syntax = typeof options.syntax === 'string' ? req(options.syntax) : options.syntax
-      }
-      const promise = processor.process(contents, processOptions).then(function (result) {
-        files[file].contents = Buffer.from(result.css)
-        debug.info('Updated CSS at "%s"', file)
-        if (map.inline) {
-          if (prevMap) {
-            debug.info('Moving contents of previous source map file "%s" inline', file)
-            delete files[`${file}.map`]
+          debug.info('Processing file "%s"', file)
+          const processOptions = {
+            from: absolutePath,
+            to: absolutePath,
+            map: mapOpts
           }
-        } else if (result.map) {
-          debug.info('%s source map at "%s"', prevMap ? 'Updating' : 'Adding', file)
-          files[`${file}.map`] = {
-            contents: Buffer.from(result.map.toString()),
-            mode: files[file].mode,
-            stats: files[file].stats
-          }
-        }
-      })
 
-      promises.push(promise)
-    })
+          if (syntax) processOptions.syntax = syntax
 
-    Promise.all(promises)
-      .then(() => {
-        debug('Finished processing %s CSS file(s)', styles.length)
-        done()
+          const promise = processor.process(contents, processOptions).then(function (result) {
+            files[file].contents = Buffer.from(result.css)
+            debug.info('Updated CSS at "%s"', file)
+            if (map.inline) {
+              if (prevMap) {
+                debug.info('Moving contents of previous source map file "%s" inline', file)
+                delete files[`${file}.map`]
+              }
+            } else if (result.map) {
+              debug.info('%s source map at "%s"', prevMap ? 'Updating' : 'Adding', file)
+              files[`${file}.map`] = {
+                contents: Buffer.from(result.map.toString()),
+                mode: files[file].mode,
+                stats: files[file].stats
+              }
+            }
+          })
+
+          promises.push(promise)
+        })
+
+        Promise.all(promises)
+          .then(() => {
+            debug('Finished processing %s CSS file(s)', styles.length)
+            done()
+          })
+          .catch((error) => {
+            // JSON.stringify on an actual error object yields 0 key/values
+            if (error instanceof Error) {
+              return done(error)
+            }
+            /* c8 ignore next */
+            done(new Error('Error during postcss processing: ' + JSON.stringify(error)))
+          })
       })
-      .catch((error) => {
-        // JSON.stringify on an actual error object yields 0 key/values
-        if (error instanceof Error) {
-          return done(error)
-        }
-        /* c8 ignore next */
-        done(new Error('Error during postcss processing: ' + JSON.stringify(error)))
-      })
+      .catch(done)
   }
 }
 
